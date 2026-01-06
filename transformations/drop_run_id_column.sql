e2a41983d0bbbe621b0d9a8782741e98fd320e59
Jesse Ocon <jesse.ocon@growthnirvana.com>
2026-01-05 17:41:44 -0800

    Updated package to have better casting

--- transformations/drop_run_id_column.sql
+++
@@ -1,8 +0,0 @@
--- Drop run_id column from reporting tables
-{% assign target_dataset = vars.target_dataset_id %}
-{% assign reporting_tables = 'campaign_performance_report,ad_performance_report,ad_group_performance_report,search_query_performance_report,keyword_performance_report,goals_and_funnels_report,ad_extension_detail_report' | split: ',' %}
-
-{% for table in reporting_tables %}
-  ALTER TABLE `{{target_dataset}}.{{table}}`
-  DROP COLUMN IF EXISTS run_id;
-{% endfor %} 
